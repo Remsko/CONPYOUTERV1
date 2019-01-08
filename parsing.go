8dd4ba26cf79c053edf6b5e95611795e4847448b
Remsko <pinoit.remi@gmail.com>
2019-01-08 15:37:03 +0100

    feat: awesome parsing

--- parsing.go
+++ parsing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -28,15 +29,85 @@ func splitEqual(equation string) (string, string) {
 	return split[0], split[1]
 }
 
+func isSign(token string) bool {
+	return token == "+" || token == "-"
+}
+
+func nextNumber(tokens []string, length *int, index int) (int, error) {
+
+	neg := false
+
+	for ; isSign(tokens[index+*length]) && index+*length < len(tokens); *length++ {
+		if tokens[index+*length] == "-" {
+			neg = (neg == false)
+		}
+	}
+	number, err := strconv.ParseInt(tokens[index+*length], 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	if neg != false {
+		number = -number
+	}
+	return int(number), nil
+}
+
+func nextDegree(tokens []string, length *int, index int) (int, error) {
+
+	err := fmt.Errorf("Wrong equation format for degree")
+	/*
+		if index+*length >= len(tokens)-2 {
+			return 0, err
+		}
+	*/
+	if tokens[index+*length] != "*" {
+		return 0, err
+	}
+	*length++
+	split := strings.Split(tokens[index+*length], "^")
+	*length++
+	if len(split) != 2 {
+		return 0, err
+	}
+	if split[0] != "X" {
+		return 0, err
+	}
+	degree, err := strconv.ParseInt(split[1], 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return int(degree), nil
+}
+
 func parseQuadratic(member string) []int {
 
+	var length int
+
 	tokens := strings.Fields(member)
+	ret := make([]int, 3)
 
-	for i := 0; i < len(tokens); i++ {
-
+	for i := 0; i < len(tokens); i += length {
+		length = 0
+		if i > 0 && isSign(tokens[i]) == false {
+			return nil
+		}
+		number, err := nextNumber(tokens, &length, i)
+		if err != nil {
+			fmt.Print(err)
+			return nil
+		}
+		length++
+		degree, err := nextDegree(tokens, &length, i)
+		if err != nil {
+			fmt.Print(err)
+			return nil
+		}
+		fmt.Print("Number: ")
+		fmt.Println(number)
+		fmt.Print("Degree: ")
+		fmt.Println(degree)
+		ret[degree] = number
 	}
-	fmt.Println(tokens)
-	ret := []int{0, 1, 2}
 
 	return ret
 }
